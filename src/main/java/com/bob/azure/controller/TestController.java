ca87ebef1a0d3ddc7856698ecc05a3bc61ff2d06
Volodymyr_Stadnyk <Volodymyr.Stadnyk@medecision.com>
2026-03-16 16:52:15 +0200

    Added initial commit.

---
+++ src/main/java/com/bob/azure/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.bob.azure.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+    @GetMapping
+    public String test() {
+        log.info("[test] method called");
+        return "test done";
+    }
+}
